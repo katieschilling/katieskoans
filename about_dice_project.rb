085a10d11851b745aadd6fe2762f5eee20cac215
Princess Katie <kdschilling@gmail.com>
2014-09-22 15:40:43 -0500

    Define dice class and generate array of rolls of any length

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+  def roll(num_rolls)
+    @values = 1.upto(num_rolls).map{ |num_rolls| rand(6) + 1 }
+    values
+  end
+    #   code ...
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
